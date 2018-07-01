f1af6e7cd9d030cac719a63b86d9c872d846dcd4
lubaijie_bm <lubaijie_bm@qq.com>
2018-07-01 21:19:12 +0800

    2018-07-01
    Signed-off-by: lubaijie_bm <lubaijie_bm@qq.com>

---
+++ MVC/SpaceHome/SpaseHome.NewWeb/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaseHome.NewWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Receive()
+        {
+            int i = 0;
+            return Content("success");
+        }
+    }
+}
